6298c0e41fb0b1dcbbc7181ec2073fe7f953b2e5
Sebastian Pareja <spatencia@gmail.com>
2019-09-17 17:09:19 -0500

    first commit

---
+++ DataAccess/Connection.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DataAccess.Conexion
+{
+    public class Connection
+    {
+        string ConnectionString = "Data Source=10.231.50.69; Initial Catalog=SPA; Integrated Security=False; User Id=Kmendoza;Password=kmendoza2018; MultipleActiveResultSets=True";
+        SqlConnection con;
+
+        public void OpenConection()
+        {
+            con = new SqlConnection(ConnectionString);
+            con.Open();
+        }
+
+        public void CloseConnection()
+        {
+            con.Close();
+        }
+
+        /// <summary>
+        /// Para insertar, Actualizar o Eliminar
+        /// </summary>
+        /// <param name="Query_"></param>
+        public bool ExecuteQueries(string Query_)
+        {
+            SqlCommand cmd = new SqlCommand(Query_, con);
+            return cmd.ExecuteNonQuery() > 0 ? true : false;
+        }
+
+        /// <summary>
+        /// Para los comandos Select
+        /// </summary>
+        /// <param name="Query_"></param>
+        /// <returns></returns>
+        public SqlDataReader DataReader(string Query_)
+        {
+            try
+            {
+                SqlCommand cmd = new SqlCommand(Query_, con);
+                SqlDataReader dr = cmd.ExecuteReader();
+                return dr;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Retornar en una tabla el resultado de la consulta (SELECT)
+        /// </summary>
+        /// <param name="Query_"></param>
+        /// <returns></returns>
+        public DataTable ShowDataTable(string Query_)
+        {
+            try
+            {
+                SqlDataAdapter dr = new SqlDataAdapter(Query_, ConnectionString);
+                DataSet ds = new DataSet();
+                dr.Fill(ds);
+                DataTable dataum = ds.Tables[0];
+                return dataum;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+    }
+}
---
+++ DataAccess/EmpleadoDataAccess.cs
@@ -0,0 +1,70 @@
+﻿using DataAccess.Conexion;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace DataAccess
+{
+    public class EmpleadoDataAccess
+    {
+
+        
+        public static DataTable ObtenerEmpleado(string nombre)
+        {
+            Connection con = new Connection();
+            DataTable result = new DataTable();
+            try
+            {
+                con.OpenConection();
+                result = con.ShowDataTable("SELECT * FROM Empleado WHERE Nombre = " + nombre);
+                con.CloseConnection();
+            }
+            catch (Exception)
+            {
+                con.CloseConnection();
+                return new DataTable();
+            }
+            return result;
+        }
+
+        public static DataTable ObtenerTodosEmpleados()
+        {
+            Connection con = new Connection();
+            DataTable result = new DataTable();
+            try
+            {
+                con.OpenConection();
+                result = con.ShowDataTable("SELECT * FROM Empleado");
+                con.CloseConnection();
+            }
+            catch (Exception)
+            {
+                con.CloseConnection();
+                return new DataTable();
+            }
+            return result;
+        }
+
+        public static bool InsertarEmpleado(string nombre, string apellido, int salario, int tipoContrato)
+        {
+            Connection con = new Connection();
+            bool result = false;
+            try
+            {
+                con.OpenConection();
+                string QueryInsert = string.Format("INSERT INTO Empleado(Nombre, Apellido, Salario, TipoContrato) values ('{0}','{1}',{2},{3})", nombre, apellido, salario, tipoContrato);
+                result = con.ExecuteQueries(QueryInsert);
+                con.CloseConnection();
+            }
+            catch (Exception)
+            {
+                con.CloseConnection();
+                result = false;
+            }
+            return result;
+        }
+
+
+    }
+}
